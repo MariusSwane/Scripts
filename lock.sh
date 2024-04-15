046cb26c3838cc34a15903b89297c8b4745bf891
Marius Swane <marius.wishman@ntnu.no>
2024-04-15 13:02:05 +0200

    Mon Apr 15 01:02:05 PM CEST 2024

---
+++ lock.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+swaylock \
+	--clock \
+	--indicator \
+	--effect-blur 7x5 \
+	--ring-color 00000050 \
+	--inside-color 00000050 \
+	--text-clear-color 000000\
+	--line-color 00000000 \
+	--separator-color 00000000 \
+	--text-color ffffffee \
+	--text-ver-color 00000000 \
+	--text-wrong-color 00000000 \
+	--font CMUSerif	\
+	--font-size 26 \
+	-i .local/share/bg.jpg
